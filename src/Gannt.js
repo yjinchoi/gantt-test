1252f73f095e22f4d211612644eede23a3462d93
Yong-Jin Choi <yjinchoi@ariatech.kr>
2022-02-20 21:20:37 +0900

    onBefreLightBox 테스트

--- src/Gannt.js
+++ src/Gannt.js
@@ -2,7 +2,7 @@
 import { gantt } from "dhtmlx-gantt";
 import { useEffect } from "react";
 
-import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
+import 'dhtmlx-gantt/codebase/skins/dhtmlxgantt_material.css';
 
 function Gantt ( props ) {
 
@@ -10,8 +10,43 @@ function Gantt ( props ) {
 
     useEffect ( () => {
 
+        gantt.attachEvent("onBeforeLightbox", function(id) {
+
+            let task = gantt.getTask ( id );
+
+            if ( task.$new ) {
+
+                gantt.deleteTask ( id );
+                return false;
+
+            }
+
+            return false;
+
+        });
+
+        gantt.config.layout = {
+            css: "gantt_container",
+            rows:[
+              {
+                cols: [
+                  {view: "grid", id: "grid", scrollX:"scrollHor", scrollY:"scrollVer"},
+                  {view: "timeline", id: "timeline", scrollX:"scrollHor", scrollY:"scrollVer"},
+                  {view: "scrollbar", scroll: "y", id:"scrollVer"}
+                ]
+               },
+              {view: "scrollbar", scroll: "x", id:"scrollHor", height:20}
+            ]
+        };
+        
         gantt.init ( id );
         gantt.parse ( tasks );
+
+        return (
+
+            () => gantt.detachAllEvents ()
+
+        );
 
     }, [] );
 
